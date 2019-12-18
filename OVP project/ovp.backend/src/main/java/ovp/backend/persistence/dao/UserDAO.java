fda0559c0b53d67b04f585ab0ca5cb15c0c33be4
Razvan Barbulescu <barbulescu.razvan169@gmail.com>
2019-12-18 17:34:19 +0200

    version 1.6 Business layer added

--- OVP project/ovp.backend/src/main/java/ovp/backend/persistence/dao/UserDAO.java
+++ OVP project/ovp.backend/src/main/java/ovp/backend/persistence/dao/UserDAO.java
@@ -8,9 +8,9 @@ public interface UserDAO {
 
 	void createUser(User user);
 	void updateUser(String email, String userName, String password, int userTypeId);
-	void updatePassword(String userName, String oldPassword, String newPassword, int userTypeId);
+	void updatePassword(String userName, String oldPassword, String newPassword);
 	void deleteUser(String userName);	
-	User loginCheck(String userName, String password, int userTypeId);
+	User loginCheck(String userName, String password);
 	User findUserByUserName(String userName);
 	List<User> displayUsers();
 	
